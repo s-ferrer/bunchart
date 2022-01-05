338c267ca3f0cb38ffeee834141504642bcd108e
s-ferrer <saraferrer@gmail.com>
2022-01-05 18:53:27 +0100

    Create auction.js class file

---
+++ auction.js
@@ -0,0 +1,10 @@
+class Auction {
+    constructor(category, startprice) {
+    this.category = category
+    this.startprice = startprice
+    this.endprice = endprice
+    this.bidders = []
+    }
+}
+
+module.exports = Auction;
