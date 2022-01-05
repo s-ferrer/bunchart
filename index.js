c83632c35eff3f2a405c6afcca644e4e495b9d8c
s-ferrer <saraferrer@gmail.com>
2022-01-05 18:29:01 +0100

    Create index.js file

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+const Person = require("./person");
+const Artwork = require("./artwork");
 
-class Person {
-    constructor(name, age) {
-        this.name = name
-        this.age = age
-        this.photos = []
-    }
-}
+const mihri = new Person('mihri', 35, 'artist');
+const manolo = new Person('manolo', 21, 'collector');
+const sara = new Person('sara', 36, 'artist');
 
-const eli = new Person('eli', 34)
+const sculpture01 = new Artwork("sculpture01.jpg", "wood", 2021, 2500);
+const painting01 = new Artwork("painting01.jpg", "acrylic", 2020, 2700);
+sara.addArt(sculpture01);
+sara.addArt(painting01);
+
+sara.bio = 
+'An International Abstract and Conceptual Artist'
+
+manolo.likeArt(painting01);
+mihri.likeArt(painting01);
+
+
+console.log(sara.profile);
