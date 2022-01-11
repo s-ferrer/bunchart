5faa2372a52b70d2ad6ffc7ef95012f44fe2799a
s-ferrer <saraferrer@gmail.com>
2022-01-11 09:28:42 +0100

    Add new methods in class Auction

--- src/auction.js
+++ src/auction.js
@@ -1,8 +1,25 @@
 class Auction {
-  constructor(category, startprice) {
+  constructor(category) {
     this.category = category
-    this.startprice = startprice
     this.bidders = []
+    this.presentedArtworks = []
+  }
+
+  presentArtwork(artworkId, startPrice) {
+    this.presentedArtworks.push({
+      artworkId,
+      startPrice,
+      bids: [],
+    })
+  }
+
+  bid(personId, artworkId, offer) {
+    const presentedArtwork = this.presentArtworks.find(pA => pA.artworkId == artworkId)
+    if (!presentedArtwork) throw new Error('Artwork not found')
+
+    // Check if the offer is higher than the previous one
+
+    presentedArtwork.bids.push({ personId, offer })
   }
 }
 
