c1efd13be42577d185117f094680c965ffc69f8d
s-ferrer <saraferrer@gmail.com>
2022-01-17 21:04:32 +0100

    Add router.post in artworks.js

--- src/routes/artworks.js
+++ src/routes/artworks.js
@@ -1,10 +1,27 @@
 /* eslint-disable func-names */
 const express = require('express')
 
+const Artwork = require('../models/artwork')
+
 const router = express.Router()
-// GET /artworks
-router.get('/', (req, res) => {
-  res.send([])
+
+/* POST add an artwork. */
+// eslint-disable-next-line no-unused-vars
+router.post('/', function (req, res, next) {
+  const { filenamen, material, year, price } = req.body
+
+  if (!filenamen || !material || !year || !price) {
+    res
+      .send({
+        message: 'Missing fields.',
+      })
+      .status(400)
+    return
+  }
+
+  const myArtwork = new Artwork(filenamen, material, year, price)
+
+  res.send(myArtwork)
 })
 
 module.exports = router
