0a94d86b0c8a34171e79cc6ec16f801de4099a7b
s-ferrer <saraferrer@gmail.com>
2022-01-18 12:32:11 +0100

    Add require Artwork, Auction and Person

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,14 @@ const express = require('express')
 // 2 res.send recibes and return .json to interact with the data
 const router = express.Router()
 
+// const Artwork = require('../models/artwork')
+
+// const Auction = require('../models/auction')
+
+// const Person = require('../models/person')
+
+// const dena = new Person('Dena', 38, 'artist')
+
 /* GET home page. */
 // eslint-disable-next-line no-unused-vars
 router.get('/', function (req, res, next) {
