0a931c92249c36b95309de86c25ab95437ad1d91
s-ferrer <saraferrer@gmail.com>
2022-01-19 19:27:55 +0100

    Add require database-connection

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
+require('./database-connection')
+
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 const artworksRouter = require('./routes/artworks')
@@ -28,13 +30,13 @@ app.use('/artworks', artworksRouter)
 app.use('/auctions', auctionsRouter)
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404))
 })
 
 // error handler
 // eslint-disable-next-line no-unused-vars
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
