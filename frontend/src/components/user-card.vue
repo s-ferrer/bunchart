0bee16d9e2cbed37b354d99d8d5c41a3fffe2a79
s-ferrer <saraferrer@gmail.com>
2022-01-27 19:39:08 +0100

    Add card to user.card.vue

--- frontend/src/components/user-card.vue
+++ frontend/src/components/user-card.vue
@@ -1,12 +1,23 @@
 <script>
 export default {
   name: 'UserCard',
+  props: ['user'],
 }
 </script>
 
 <template lang="pug">
-.box
-    h2 user name ( age)
+  .box
+    h1 {{ user.name }} ({{ user.age }}) {{ user.profession }}
+    p Bio: {{ user.bio || 'This user has not bio yet.' }}
+    h2 Artworks ({{ user.artworksList.length }})
+
+    .artwork(v-for="artwork in user.artworksList")
+        h3 {{ artwork.artworkName }}
+        p(v-if="!artwork.likedBy.length")
+          | no likes yet!
+        p(v-else)
+          | {{ artwork.likedBy.map(user => user.name).join(', ') }}
+
 </template>
 
 <style lang="scss" scoped>
@@ -16,5 +27,6 @@ export default {
   border: 1px solid rgb(36, 25, 190);
   background: rgb(81, 115, 209);
   border-radius: 0.3rem;
+  // text-align: center;
 }
 </style>
