a9d8a095d3d4b613dfbe73f2bdc799eba5a98f91
s-ferrer <saraferrer@gmail.com>
2022-01-27 17:47:35 +0100

    Create frontend folder. Add vue files

---
+++ frontend/src/components/user-card.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'UserCard',
+}
+</script>
+
+<template lang="pug">
+.box
+    h2 user name ( age)
+</template>
+
+<style lang="scss" scoped>
+.box {
+  color: white;
+  padding: 2rem;
+  border: 1px solid rgb(36, 25, 190);
+  background: rgb(81, 115, 209);
+  border-radius: 0.3rem;
+}
+</style>
---
+++ frontend/src/views/home.vue
@@ -0,0 +1,30 @@
+<script>
+import axios from 'axios'
+import UserCard from '@/components/user-card.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    UserCard,
+  },
+  data() {
+    return {
+      users: [],
+    }
+  },
+  async created() {
+    const usersRequest = await axios.get('/api/users')
+
+    this.users = usersRequest.data
+  },
+}
+</script>
+
+<template lang="pug">
+  .home
+    h1 bunchart
+    user-card
+    img(alt="bunchart logo" src="../assets/logo-bunchart-temp.png")
+    div(v-for="user in users") {{ user.name }} has {{ user.artworksList.length }} artworksList
+
+</template>
