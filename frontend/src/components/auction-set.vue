9789fa14771b9c68abfdcd96401d5e2e33e20cfd
s-ferrer <saraferrer@gmail.com>
2022-01-31 08:56:32 +0100

    Add auction-set.vue file

---
+++ frontend/src/components/auction-set.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: 'AuctionSet',
+  props: ['artwork'],
+}
+</script>
+
+<template lang="pug">
+  .box
+    h1 {{ artwork.artworkName }} {{ artwork.filename }} {{ artwork.material }} {{ artwork.price }}
+
+</template>
+
+<style lang="scss" scoped>
+.box {
+  color: white;
+  padding: 2rem;
+  border: 1px solid rgb(36, 25, 190);
+  background: rgb(81, 115, 209);
+  border-radius: 0.3rem;
+  text-align: center;
+}
+</style>
