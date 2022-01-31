ea8d9fb6ee76cf8e72121910a9acb3314838b25e
s-ferrer <saraferrer@gmail.com>
2022-01-31 19:45:43 +0100

    Change router link for user list

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,8 +1,7 @@
 <template lang="pug">
   #app
     #nav
-      router-link(to="/") Home |&nbsp;
-      router-link(to="/about") About |&nbsp;
+      router-link(to="/") User List | &nbsp;
       router-link(to="/auction") Auction
     router-view
 </template>
