327d56ac3629a976e661098af37b028578b35013
s-ferrer <saraferrer@gmail.com>
2022-02-04 10:49:48 +0100

    Remove unused code

--- backend/src/lib/image-description.js
+++ backend/src/lib/image-description.js
@@ -9,7 +9,6 @@ const { Apikey } = defaultClient.authentications
 Apikey.apiKey = process.env.BUNCHART_APIKEY
 // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
 // Apikey.apiKeyPrefix = 'Token';
-
 const apiInstance = new CloudmersiveImageApiClient.RecognizeApi()
 
 // const imageFile = '/path/to/file.txt' // File | Image file to perform the operation on.  Common file formats such as PNG, JPEG are supported.
