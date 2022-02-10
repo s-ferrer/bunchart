7db17e5a5554b8e90e92368ea4c5d6df7c1203f1
s-ferrer <saraferrer@gmail.com>
2022-02-10 22:54:21 +0100

    Import socket.io and add mutations in index.js store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,25 +2,34 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 
-// import io from 'socket.io-client'
+import io from 'socket.io-client'
 
 axios.defaults.baseURL = process.env.VUE_APP_BASE_URL
 axios.defaults.withCredentials = true
 
 Vue.use(Vuex)
 
-// const socket = io(process.env.VUE_APP_BASE_URL)
+const socket = io(process.env.VUE_APP_BASE_URL)
+
 const mutations = {
-  INCREMENT_COUNT: 'incrementCount',
+  INCREMENT_COUNT: 'increment count',
+  SET_USER: 'set user',
+  SET_LIVE_STREAM: 'set live stream',
+  ADD_LIVE_STREAM: 'add live stream',
+  ADD_MESSAGE_TO_LIVE_STREAM: 'add message to live stream',
 }
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   state: {
     count: 0,
+    user: null,
   },
   mutations: {
     [mutations.INCREMENT_COUNT](state) {
       state.count++
+    },
+    [mutations.SET_USER](state, user) {
+      state.user = user
     },
   },
   actions: {
@@ -35,6 +44,30 @@ export default new Vuex.Store({
       const usersRequest = await axios.get('/api/users')
       return usersRequest.data
     },
+    async fetchSession({ commit }) {
+      const user = await axios.get('/api/account/session')
+      commit(mutations.SET_USER, user.data || null)
+    },
+    async login({ commit }, credentials) {
+      try {
+        const user = await axios.post('/api/account/session', credentials)
+        commit(mutations.SET_USER, user.data)
+      } catch (e) {
+        throw e
+      }
+    },
+    async register(store, user) {
+      return axios.post('/api/account', user)
+    },
+    async logout({ commit }) {
+      await axios.delete('/api/account/session')
+      commit(mutations.SET_USER, null)
+    },
   },
   modules: {},
 })
+
+export default async function init() {
+  await store.dispatch('fetchSession')
+  return store
+}
