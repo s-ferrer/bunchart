1d74de7e741b2689b860da3fda6ad9c7ea0ecda5
s-ferrer <saraferrer@gmail.com>
2022-02-10 22:55:21 +0100

    Import login, register and profile.vue in index.js router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Profile from '../views/profile.vue'
+import Login from '../views/login.vue'
+import Register from '../views/register.vue'
+
 import UserList from '../views/user-list.vue'
 
 Vue.use(VueRouter)
@@ -17,6 +21,33 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/user-detail.vue'),
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register,
+    beforeEnter(to, from, next) {
+      if (store.state.user) return next('/profile')
+      return next()
+    },
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+    beforeEnter(to, from, next) {
+      if (store.state.user) return next('/profile')
+      return next()
+    },
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: UserList,
+    beforeEnter(to, from, next) {
+      if (store.state.user) return next('/login')
+      return next()
+    },
   },
   {
     path: '/auction',
