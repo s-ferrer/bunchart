c93757c22f7c73fc4380c532dd1ce34fa2a2e7bd
s-ferrer <saraferrer@gmail.com>
2022-02-12 15:01:03 +0100

    Create login and home.vue files

---
+++ frontend/src/views/home.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<template lang="pug">
+    .home
+        img(src='../assets/logo-bunchart-temp.png' alt='logo' style='width:100px; height:100px;')
+        h1 bunchart
+        h2 Welcome
+        .box
+          h3 Support your local artists!
+</template>
+
+<style lang="scss" scoped>
+.home {
+  background-color: rgb(180, 180, 180);
+  padding: 1rem;
+  h1 {
+    margin: 2rem;
+  }
+  h2 {
+    margin: 3rem;
+  }
+}
+.box {
+  background-color: rgb(160, 101, 101);
+  padding: 1rem;
+  h3 {
+    background-color: rgb(119, 101, 160);
+    margin: 4rem;
+  }
+}
+</style>
