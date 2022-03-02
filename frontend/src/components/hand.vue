b06cbfe7534a80f31692d330dcc55269e8260477
s-ferrer <saraferrer@gmail.com>
2022-03-02 19:23:41 +0100

    Create hand component. In process

---
+++ frontend/src/components/hand.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'Hand',
+  props: [''],
+}
+</script>
+
+<template lang="pug">
+        <button v-on:click="counter += 20">Add 1</button>
+</template>
+
+<style lang="scss" scoped>
+a {
+  background: greenyellow;
+  color: #162c40;
+  padding: 0.7rem;
+  border: solid 1px #162c40;
+  border-radius: 50%;
+  text-decoration: none;
+}
+</style>
