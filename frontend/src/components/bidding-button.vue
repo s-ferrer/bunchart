d2cd7a82bd8b6c9f42201585930694b4bec52882
s-ferrer <saraferrer@gmail.com>
2022-03-02 19:24:19 +0100

    Create bidding-button

--- frontend/src/components/bidding-button.vue
+++ frontend/src/components/bidding-button.vue
@@ -1,1 +1,21 @@
-button(@click="") Start bidding
+<script>
+export default {
+  name: 'BiddingButton',
+  props: [''],
+}
+</script>
+
+<template lang="pug">
+        a(@click="" href="/bid") Bid now!
+</template>
+
+<style lang="scss" scoped>
+a {
+  background: greenyellow;
+  color: #162c40;
+  padding: 0.7rem;
+  border: solid 1px #162c40;
+  border-radius: 50%;
+  text-decoration: none;
+}
+</style>
