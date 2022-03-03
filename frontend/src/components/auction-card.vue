7ec8e3c691b506ca060c3bc49fd73cdfbc4b1483
s-ferrer <saraferrer@gmail.com>
2022-03-03 17:44:19 +0100

    Update auction-card component

--- frontend/src/components/auction-card.vue
+++ frontend/src/components/auction-card.vue
@@ -6,40 +6,43 @@ export default {
 </script>
 
 <template lang="pug">
-  .box1
-    .container1
-      p {{ auction.category }}
-      img(:src="require(`@/assets/${this.auction.category}.jpg`)" :alt="`This is the ${this.auction.category} image`")
+  #app
+    .container
+      .row
+        .col-12
+          .text
+            p {{ auction.category }}
+        .col-12
+          img(:src="require(`@/assets/${this.auction.category}.jpg`)" :alt="`This is the ${this.auction.category} image`")
 
 </template>
 
 <style lang="scss" scoped>
-.box1 {
-  margin: 1%;
-  padding: 0.7rem;
-  .container1 {
-    p {
-      color: #05dbf2;
-      font-size: 2rem;
-      padding: 3px;
-      margin: 2px 40px 4px 40px;
-    }
-    button {
-      border-radius: 0.7rem;
-      padding: 0.7rem;
-      margin: 20px;
-      background-color: #2e8bc0;
-      color: #ffffff;
-      font-size: 1rem;
-    }
+.container {
+  // background-color: #162c40;
+  margin: auto;
+  padding: 0rem;
+  width: 100%;
+  height: auto;
+
+  .text {
+    display: flex;
+    color: #05dbf2;
+    justify-content: center;
+    font-size: 1.7rem;
+    padding-top: 10%;
   }
+
   img {
     border-radius: 10px;
-    border-style: solid;
-    border-width: 1px;
-    border-color: #05f2f2;
-    width: 350px;
-    height: 350px;
+    border: solid 1px #05f2f2;
+    width: 15rem;
+    height: 15rem;
   }
+}
+p {
+  color: #05dbf2;
+  font-size: 2rem;
+  padding-top: 2px;
 }
 </style>
