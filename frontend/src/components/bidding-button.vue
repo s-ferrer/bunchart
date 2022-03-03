e223cdc96e0a11eea01d88aa2da68d16d3eafdf7
s-ferrer <saraferrer@gmail.com>
2022-03-03 17:43:38 +0100

    Style bidding-button component

--- frontend/src/components/bidding-button.vue
+++ frontend/src/components/bidding-button.vue
@@ -6,16 +6,25 @@ export default {
 </script>
 
 <template lang="pug">
+#app
+    .container
+      .row
         a(@click="" href="/bid") Bid now!
 </template>
 
 <style lang="scss" scoped>
 a {
-  background: greenyellow;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgb(228, 205, 0);
   color: #162c40;
-  padding: 0.7rem;
-  border: solid 1px #162c40;
-  border-radius: 50%;
+  padding: 0.5rem;
+  border: solid 1px #05f2f2;
+  border-radius: 150%;
+  margin-top: 15%;
+  width: 90%;
+  height: 100%;
   text-decoration: none;
 }
 </style>
