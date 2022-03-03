33b2a4d1198487dade0181b6e2f8c825dea71a03
s-ferrer <saraferrer@gmail.com>
2022-03-03 17:42:56 +0100

    Create new bidding-card.vue component

---
+++ frontend/src/components/bidding-card.vue
@@ -0,0 +1,53 @@
+<script>
+import BiddingButton from '@/components/bidding-button.vue'
+
+export default {
+  name: 'BiddingCard',
+  props: ['auction'],
+  components: { BiddingButton },
+}
+</script>
+
+<template lang="pug">
+  #app
+    .container
+      .row
+        .col-6
+            .text
+                p {{ auction.category }}
+        .col-6
+            .button
+                BiddingButton
+        .col-12
+          img(:src="require(`@/assets/${this.auction.category}.jpg`)" :alt="`This is the ${this.auction.category} image`")
+
+</template>
+
+<style lang="scss" scoped>
+.container {
+  // background-color: #162c40;
+  margin: auto;
+  padding: 0rem;
+  width: 100%;
+  height: auto;
+
+  .text {
+    display: flex;
+    color: #05dbf2;
+    font-size: 1.7rem;
+    padding-left: 25%;
+    padding-top: 18%;
+    align-items: flex-start;
+  }
+  .button {
+    padding-top: 5%;
+    font-weight: 700;
+  }
+  img {
+    border-radius: 10px;
+    border: solid 1px #05f2f2;
+    width: 15rem;
+    height: 15rem;
+  }
+}
+</style>
